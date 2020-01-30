7463a384fd96b0547404cf82b379207adc6233e9
Shin seongsu <tlstjdtn321@naver.com>
2020-01-30 23:14:15 +0900

    MainDao

--- src/main/java/com/spring/shop/DAO/MainDAO.java
+++ src/main/java/com/spring/shop/DAO/MainDAO.java
@@ -1,6 +1,7 @@
 package com.spring.shop.DAO;
 
 import java.util.List;
+import java.util.Map;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,5 +16,9 @@ public class MainDAO {
 	
 	public List<MainVO> MainList() {
 		return mybatis.selectList("Main-Mapping.MainList");
+	}
+	
+	public List<Map<String, String>> Categories() {
+		return mybatis.selectList("Main-Mapping.Categories");
 	}
 }
