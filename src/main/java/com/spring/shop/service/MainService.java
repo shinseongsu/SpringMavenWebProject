0d4a49f94841c7afbf28c813adaaf8f0b6ef9f1e
Shin seongsu <tlstjdtn321@naver.com>
2020-01-30 23:14:50 +0900

    Main Service

--- src/main/java/com/spring/shop/service/MainService.java
+++ src/main/java/com/spring/shop/service/MainService.java
@@ -1,9 +1,12 @@
 package com.spring.shop.service;
 
 import java.util.List;
+import java.util.Map;
 
 import com.spring.shop.VO.MainVO;
 
 public interface MainService {
 	public List<MainVO> MainList();
+	
+	public List<Map<String, String>> Categories();
 }
